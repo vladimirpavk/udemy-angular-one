35663e608c025c15f7530af74059a4add6a73f34
Vladimir Pavković <vladimirpavk@gmail.com>
2018-02-27 14:59:16 +0100

    Components

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+// tslint:disable-next-line:eofline
+// tslint:disable-next-line:no-trailing-whitespace
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+export class ServerComponent{
+    public title = 'My Server Component';
+}
