54e81e7b5a2559d541207c21d8b14d98ba7f562f
Vladimir Pavković <vladimirpavk@gmail.com>
2018-03-06 10:45:22 +0100

    +

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+class Model{
+  public id:number;
+  public date:Date;
+  constructor(id_1:number){
+    this.id=id_1;
+    this.date=new Date();
+  }
+}
+
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  //styleUrls: ['./servers.component.css']
+  styles:[`
+    .klasa1{
+      color: white
+    }
+  `]
 })
 export class ServersComponent implements OnInit {
 
-  private isEnabled: boolean = false;
+ /* private isEnabled: boolean = false;
   private username: string = '';
 
   constructor() { }
@@ -33,5 +47,35 @@ export class ServersComponent implements OnInit {
   buttonClicked(){
     this.username='';
     console.log("button clicked");
+  }*/
+
+  constructor() { }
+  
+  public isVisible:boolean = false;
+  public models: Array<Model> = [];
+  public intervals: number=0;
+  public isBGBlue = false;
+
+  ngOnInit() {
+
+  }
+
+  toggleVisible(){
+    this.isVisible=!this.isVisible;
+    //if(this.intervals > 5) this.isBGBlue=true;
+    this.models.push(new Model(this.intervals++));
+    //console.log(this.models);
+  }
+
+  setStyle(){
+    return{
+      'background-color': this.intervals > 5 ? 'blue' : 'white'
+    }
+  }
+
+  setKlasa(){
+    return{
+      'klasa1' : this.isBGBlue
+    }
   }
 }
